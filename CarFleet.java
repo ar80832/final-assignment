03ad45100a7843b8318db2cca40021dd79ea1a78
ar80832 <69652822+ar80832@users.noreply.github.com>
2023-01-19 14:14:53 -0800

    Update CarFleet.java

--- CarFleet.java
+++ CarFleet.java
@@ -3,7 +3,8 @@ package assignment5;
 //import java.util.Collection;
 //import java.util.LinkedList;
 
-//import java.util.LinkedList;
+import java.util.LinkedList;
+import java.util.List;
 
 public class CarFleet {
 	private Queue gasCars;
@@ -34,21 +35,21 @@ public class CarFleet {
 	
 	}
 	
-	public Queue processRequests(Queue req) {
-		Queue customerOrder = new Queue();
+	public List<Object> processRequests(Queue req) {
+		List<Object> customerOrder = new LinkedList<>();
 		for (int i = 0; i < req.getSize(); i++) {
 			if (req.getInfo(i).equals(1) && gasCars.getSize() > 0) {
-				customerOrder.enqueue(gasCars.dequeue());
+				customerOrder.add(gasCars.dequeue());
 			}
 			else if (req.getInfo(i).equals(2) && hybridCars.getSize() > 0) {
-				customerOrder.enqueue(hybridCars.dequeue());	
+				customerOrder.add(hybridCars.dequeue());	
 			}
 			else if (req.getInfo(i).equals(3) && electricCars.getSize() > 0) {
-				customerOrder.enqueue(electricCars.dequeue());
+				customerOrder.add(electricCars.dequeue());
 			}
 			else {
 				Car empty = new Car(0, 1, 1);
-				customerOrder.enqueue(empty);
+				customerOrder.add(empty);
 			}
 		}
 		return customerOrder;
